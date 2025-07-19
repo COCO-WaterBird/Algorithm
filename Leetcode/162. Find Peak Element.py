4c7ea251c714ee5dcb1847bcc3aa3eedf8707c37
COCO-WaterBird <zhangkk533@gmail.com>
2025-07-19 13:50:24 -0700

    Create 162. Find Peak Element.py

---
+++ Leetcode/162. Find Peak Element.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l = -1
+        r = len(nums) - 1
+        while l + 1 < r:
+            mid = (l + r)//2
+            if nums[mid] < nums[mid + 1]:
+                l = mid
+            else:
+                r = mid
+        return r
